bc3de3ba0f8b36325f4bdc593ac9269a46ffd91b
Matvey <veleskg@mail.ru>
2022-11-05 11:33:59 +0500

    Задание 01, Маслов Матвей ПрИ-202

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,8 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-
+        Logger logger1 = Logger.getLogger("test");
+        Logger logger2 = Logger.getLogger("test2");
+        System.out.println(logger1 == logger2);
     }
 }
